f03b7174d09ae1487d4c7b20f01faea5f20d5e4f
CynthMartz <carol.martz21@gmail.com>
2023-04-29 22:14:56 -0400

    Agregando un const para que no recargue la pagina

--- src/componentes/Formulario/Formulario.js
+++ src/componentes/Formulario/Formulario.js
@@ -4,8 +4,14 @@ import ListaOpciones from "../ListaOpciones"
 import Boton from "../Boton"
 
 const Formulario = () => {
+    const manejarEnvio = (evento) => {
+        //hace que no recargue la pagina al dar click en el boton
+        evento.preventDefault()
+        return
+    }
+
     return <section className="formulario">
-        <form>
+        <form onSubmit={manejarEnvio}>
             <h2>Rellena el formulario para crear el colaborador.</h2>
             <CampoTexto titulo="Nombre" placeholder="Ingrese nombre"/>
             <CampoTexto titulo="Puesto" placeholder="Ingrese puesto"/>
