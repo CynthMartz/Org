89555e26484ea9c4cc30305c9ae0fb3e205178e1
CynthMartz <carol.martz21@gmail.com>
2023-04-30 14:15:55 -0400

    Creando props para rellenar los campos

--- src/componentes/Formulario/Formulario.js
+++ src/componentes/Formulario/Formulario.js
@@ -1,23 +1,57 @@
+import { useState } from "react"
 import "./Formulario.css"
 import CampoTexto from "../CampoTexto"
 import ListaOpciones from "../ListaOpciones"
 import Boton from "../Boton"
 
-const Formulario = () => {
+const Formulario = (props) => {
+    const [nombre, actualizarNombre]= useState("")
+    const [puesto, actualizarPuesto]= useState("")
+    const [foto, actualizarFoto]= useState("")
+    const [equipo, actualizarEquipo] =useState("")
+
     const manejarEnvio = (evento) => {
         //hace que no recargue la pagina al dar click en el boton
         evento.preventDefault()
-        return
+        let datosAEnviar = {
+            nombre,
+            puesto,
+            foto,
+            equipo
+        }
     }
 
     return <section className="formulario">
         <form onSubmit={manejarEnvio}>
             <h2>Rellena el formulario para crear el colaborador.</h2>
-            <CampoTexto titulo="Nombre" placeholder="Ingrese nombre" required/>
-            <CampoTexto titulo="Puesto" placeholder="Ingrese puesto" required/>
-            <CampoTexto titulo="Foto" placeholder="Ingrese enlace de foto" required/>
-            <ListaOpciones/>
-            <Boton texto="Crear"/>
+            <CampoTexto
+                titulo="Nombre" 
+                placeholder="Ingrese nombre" 
+                required 
+                valor={nombre} 
+                actualizarValor={actualizarNombre}/>
+
+            <CampoTexto 
+                titulo="Puesto" 
+                placeholder="Ingrese puesto" 
+                required
+                valor={puesto} 
+                actualizarValor={actualizarPuesto}/>
+
+            <CampoTexto 
+                titulo="Foto" 
+                placeholder="Ingrese enlace de foto" required
+                valor={foto} 
+                actualizarValor={actualizarFoto}/>
+
+            <ListaOpciones 
+            valor={equipo}
+            actualizarEquipo={actualizarEquipo}
+            equipos={props.equipos}/>
+
+            <Boton>
+                Crear
+            </Boton>
         </form>
     </section>
 }
